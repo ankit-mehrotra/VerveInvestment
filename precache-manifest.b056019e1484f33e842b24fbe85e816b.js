3493eeb519044505abf3aa928020fdd2913a7f42
Ankit Mehrotra <ankmehro0@publicisgroupe.net>
2018-10-15 10:32:49 +0530

    Updates

---
+++ precache-manifest.b056019e1484f33e842b24fbe85e816b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/VerveInvestment/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b819d3624a1b85651803",
+    "url": "/VerveInvestment/static/js/runtime~main.b819d362.js"
+  },
+  {
+    "revision": "7562b4584c9b52b2b69a",
+    "url": "/VerveInvestment/static/js/main.7562b458.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/VerveInvestment/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "7562b4584c9b52b2b69a",
+    "url": "/VerveInvestment/static/css/main.dbf8e669.chunk.css"
+  },
+  {
+    "revision": "fc279899317850da055bddceede10427",
+    "url": "/VerveInvestment/index.html"
+  }
+];
